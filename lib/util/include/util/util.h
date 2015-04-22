48e8f013570b83cfabe0a14c41eadd03db6a1600
Christian Wressnegger <github@wressnegger.info>
2015-04-22 15:32:38 +0200

    adapt file headers to account for libutil being an independent project

--- lib/util/include/util/util.h
+++ lib/util/include/util/util.h
@@ -1,15 +1,15 @@
 /*
- * Salad - A Content Anomaly Detector based on n-Grams
+ * libutil - Yet Another Utility Library
  * Copyright (c) 2012-2015, Christian Wressnegger
  * --
- * This file is part of Letter Salad or Salad for short.
+ * This file is part of the library libutil.
  *
- * Salad is free software: you can redistribute it and/or modify
+ * libutil is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Salad is distributed in the hope that it will be useful,
+ * libutil is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
