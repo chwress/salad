15f05c847bba91dbf8d4268ca4385547dc9541ca
Christian Wressnegger <github@wressnegger.info>
2015-04-22 15:08:44 +0200

    provide a default configuration for libutil

---
+++ lib/util/include/util/config.h
@@ -0,0 +1,18 @@
+#ifndef UTIL_CONFIG_H_
+#define UTIL_CONFIG_H_
+
+/* #undef USE_ARCHIVES */
+/* #undef LIBARCHIVE2 */
+#define GROUPED_INPUT
+#define USE_REGEX_FILTER
+
+/* #undef USE_NETWORK */
+/* #undef ALLOW_LIVE_TRAINING */
+
+#define HAS_Z_MODIFIER
+/* #undef _XOPEN_SOURCE */
+/* #undef IOTYPE_FILES */
+
+#define USE_COLORS
+
+#endif /* UTIL_CONFIG_H_ */
