692c2cd6dee293e65bf3865e8cda9c2d18c30ecc
Christian Wressnegger <github@wressnegger.info>
2015-08-27 17:06:51 +0200

    a simple config file parser

---
+++ lib/util/include/util/simple_conf.h
@@ -0,0 +1,32 @@
+/*
+ * libutil - Yet Another Utility Library
+ * Copyright (c) 2012-2015, Christian Wressnegger
+ * --
+ * This file is part of the library libutil.
+ *
+ * libutil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libutil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+/**
+ * @file
+ */
+
+#ifndef UTIL_SIMPLE_CONF_H_
+#define UTIL_SIMPLE_CONF_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef const int(*FN_KEYVALUE)(FILE* const f, const char* const key, const char* const value, void* const usr);
+
+const int fread_config(FILE* const f, const char* const header, FN_KEYVALUE handler, void* const usr);
+
+#endif /* UTIL_SIMPLE_CONF_H_ */
---
+++ lib/util/src/simple_conf.c
@@ -0,0 +1,87 @@
+/*
+ * libutil - Yet Another Utility Library
+ * Copyright (c) 2012-2015, Christian Wressnegger
+ * --
+ * This file is part of the library libutil.
+ *
+ * libutil is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libutil is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#include <util/simple_conf.h>
+#include <util/getline.h>
+#include <util/util.h>
+
+
+char* const readline(char** buf, size_t* len, FILE* f)
+{
+	while (1)
+	{
+		const ssize_t read = getline(buf, len, f);
+		if (read < 0)
+		{
+			return NULL;
+		}
+
+		char* x = ltrim(*buf);
+		if (*x == '#') continue; // ignore comments
+
+		rtrim(x);
+		if (*x == 0x00) continue; // ignore empty lines
+
+		return x;
+	}
+}
+
+const int fread_config(FILE* const f, const char* const header, FN_KEYVALUE handle, void* const usr)
+{
+	assert(f != NULL);
+	long int pos = ftell(f);
+
+	char* line = NULL;
+	size_t len = 0;
+
+	if (header != NULL)
+	{
+		char* x = readline(&line, &len, f);
+		if (x == NULL || strcasecmp(x, header) != 0)
+		{
+			free(line);
+			fseek(f, pos, SEEK_SET);
+			return 0;
+		}
+	}
+
+	unsigned int nhandled = 0;
+	while (1)
+	{
+		char* x = readline(&line, &len, f);
+		if (x == NULL)
+		{
+			break;
+		}
+
+		char* y = strchr(x, '=');
+		if (y == NULL)
+		{
+			free(line);
+			return -1;
+		}
+
+		y[0] = 0x00;
+		char* value = ltrim(y +1);
+		rtrim(x);
+
+		if (handle(f, x, value, usr)) nhandled++;
+	}
+	free(line);
+
+	return ftell(f) -pos;
+}
